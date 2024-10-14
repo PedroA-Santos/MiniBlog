12efd40acd01b070a712256ded9e463bda319988
Pedro Antonio <eupedroassuncao@gmail.com>
2024-10-14 20:49:03 -0300

    Adicionando a Navbar do Projeto

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 
+//COMPONENTES
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='about' element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
