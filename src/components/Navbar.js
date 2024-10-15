842ce89511e2ed3ddd62069d56d8800e0fbb0e48
Pedro Antonio <eupedroassuncao@gmail.com>
2024-10-15 10:16:07 -0300

    Adicionando css da navbar, do footer e estilos gerais do projeto

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <NavLink to='/'>
+        <nav className={styles.navbar}>
+            <NavLink to='/' className={styles.brand}>
                 Mini <span>Blog</span>
 
             </NavLink>
 
-            <ul>
+            <ul className={styles.links_list}>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>
                         Home
                     </NavLink>
                 </li>
                 <li>
-                <NavLink to='/about'>
+                    <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>
                         Sobre
                     </NavLink>
                 </li>
