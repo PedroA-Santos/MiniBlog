c433955e5bd3596c9a5c35def854d003fd25d7e4
Pedro Antonio <eupedroassuncao@gmail.com>
2024-12-10 20:08:36 -0300

    Finalizando Autenticação de usuário e navegação autenticada

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,16 @@
 //CSS
 import styles from './About.module.css'
 
+import { Link } from 'react-router-dom';
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className={styles.about}>
+            <h2>Sobre o mini <span>Blog</span></h2>
+            <p>Este Projeto consiste em um blog feito com React no Front-end e Firebase no back-end</p>
+            <Link to="posts/create" className='btn'>Criar Post</Link>
         </div>
+
     );
 }
 
