a82d3ad261b181668116d4385b4f10ac638ed7ac
Pedro Antonio <eupedroassuncao@gmail.com>
2024-12-27 13:32:34 -0300

    criando o hook responsável por fazer o fetch das informações do post e aplicando ele na página de ler o post

--- src/Pages/Post/Post.js
+++ src/Pages/Post/Post.js
@@ -3,13 +3,23 @@ import styles from "./Post.module.css";
 //hooks
 
 import { useParams } from 'react-router-dom'
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 function Post() {
 
     const { id } = useParams();
+
+    const { document: post, loading } = useFetchDocument("posts", id);
     return (
         <div>
-            <h1>Post {id}</h1>
+            {loading && (
+                <h1>Carregando Post...</h1>
+            )}
+            {post && (
+                <>
+                    <h1>{post.title}</h1>
+                </>
+            )}
         </div>
     )
 }
---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+    collection,
+    doc,
+    getDoc
+} from "firebase/firestore";
+
+export const useFetchDocument = (docCollection, id) => {
+    const [document, setDocument] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+
+    useEffect(() => {
+        const loadDocument = async () => {
+            setLoading(true)
+
+
+
+            try {
+                const docRef = await doc(db, docCollection, id)
+                const docSnap = await getDoc(docRef)
+
+                setDocument(docSnap.data())
+
+                setLoading(false)
+            } catch (error) {
+                console.log(error);
+                setError(error.message)
+            }
+
+            setLoading(false)
+        }
+
+
+
+        loadDocument();
+    }, [docCollection, id]);
+
+    console.log(document);
+
+
+
+    return { document, loading, error };
+};
