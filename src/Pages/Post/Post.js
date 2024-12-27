550e17e0dd41af75574576bfc9aae55419fdb438
Pedro Antonio <eupedroassuncao@gmail.com>
2024-12-27 14:14:11 -0300

    finalizando a página de ler post e estilização da página

--- src/Pages/Post/Post.js
+++ src/Pages/Post/Post.js
@@ -11,13 +11,21 @@ function Post() {
 
     const { document: post, loading } = useFetchDocument("posts", id);
     return (
-        <div>
+        <div className={styles.post_container}>
             {loading && (
                 <h1>Carregando Post...</h1>
             )}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((tag) => (
+                            <p key={tag}><span>#</span>{tag}</p>
+                        ))}
+                    </div>
                 </>
             )}
         </div>
